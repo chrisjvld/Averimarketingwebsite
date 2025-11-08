af787dbfbc474114d0c531df40b3baec59eaf898
chrisjvld <cjamvoldhughes@gmail.com>
2025-11-08 16:30:18 +0100

    Add client-side loader for Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { VercelAnalytics } from "./components/VercelAnalytics";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+    <VercelAnalytics />
+  </StrictMode>
+);
   
